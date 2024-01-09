8bda9548df3fd6e7c8c16b8bdf24f30f2f4f372c
brizzy2007 <brightdavid951@gmail.com>
2024-01-09 12:00:28 +0100

    cloudapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
+import Navbar from './Component/Navbar'
+
+
 
 const App= () =>{
   return (
     <div>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+        <Navbar />
     </div>
   )
 }
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { IoIosClose } from "react-icons/io";
+import { IoMenu } from "react-icons/io5";
+
+const Navbar = () =>{
+  return (
+    <div>
+      <h2>  Navbar</h2>
+    </div>
+  )
+}
+
+export default Navbar
