31328dc8c07944e58a806894a3bdbe119fc89635
brizzy2007 <brightdavid951@gmail.com>
2024-01-12 11:19:45 +0100

    adding innerpag2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Home_Innerpage from '../InnerPages/Home_Innerpage'
+import Home_Innerpagetwo from '../InnerPages/Home_Innerpagetwo'
+
 
 const Home = () =>{
   return (
     <div>
       <Home_Innerpage />
+      <Home_Innerpagetwo />
     </div>
   )
 }
